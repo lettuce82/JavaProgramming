90deba711642afeea5773a8c836ba99fee59d2c9
채상희 <chaesanghui142@gmail.com>
2024-03-21 09:20:14 +0900

    serverSocket

---
+++ Day8/demo/src/main/java/com/example/Exam04.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class Exam04 {
+    public static void main(String[] args) {
+        try(ServerSocket serverSocket = new ServerSocket()) {
+            
+        } catch (IOException e) {
+            
+        }
+        
+    }
+}
